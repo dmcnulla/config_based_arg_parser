4ceea333551b6a71d20688f16a48d62d29d12f86
Dave McNulla <mcnulla@gmail.com>
2016-10-01 19:31:14 -0700

    Add basic setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Set up the package."""
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1.0',
+      description='Configuation-based Arg Parser',
+      author='Dave McNulla',
+      author_email='mcnulla@gmail.com',
+      url='https://github.com/dmcnulla/config_based_arg_parser',
+      packages=['argparse', 'yaml'],
+      )
